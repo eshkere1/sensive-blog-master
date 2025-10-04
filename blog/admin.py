5dd3c2a05f62cbdfa334623172ff0c60c28dbd32
Pavel <piv17032009@gmail.com>
2025-10-04 12:44:01 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
-from blog.models import Post, Tag, Comment
+from blog.models import Post, Comment
 
 
 @admin.register(Post)
-@admin.register(Tag)
-@admin.register(Comment)
-
-
 class PostAdmin(admin.ModelAdmin):
     search_fields=['title',]
 
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['post', 'author', 'text']
     raw_id_fields = ('post', 'author')
     search_fields = ['author__username', 'post__title', 'text']
+
